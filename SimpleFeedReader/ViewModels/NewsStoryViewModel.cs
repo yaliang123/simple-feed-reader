0c9a0d7a0c5ec7d172563ed812b6b52cedf5c838
Vaishali Raval <Vaishali.Raval@neudesic.com>
2022-01-07 18:16:26 +0530

    Included file scoped namespace for simple feed reader

--- SimpleFeedReader/ViewModels/NewsStoryViewModel.cs
+++ SimpleFeedReader/ViewModels/NewsStoryViewModel.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-namespace SimpleFeedReader.ViewModels
+namespace SimpleFeedReader.ViewModels;
+public class NewsStoryViewModel
 {
-    public class NewsStoryViewModel
-    {
-        public DateTimeOffset Published { get; set; }
+    public DateTimeOffset Published { get; set; }
 
-        public string Title { get; set; }
+    public string Title { get; set; }
 
-        public string Uri { get; set; }
-    }
+    public string Uri { get; set; }
 }
