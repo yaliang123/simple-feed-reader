1e7ee8dabae1e685370aeed280f1ad4e41f3d40d
Vaishali Raval <Vaishali.Raval@neudesic.com>
2022-01-07 18:17:13 +0530

    Included file scoped namespace for simple feed reader test

--- SimpleFeedReader.Tests/Services/NewsServiceTests.cs
+++ SimpleFeedReader.Tests/Services/NewsServiceTests.cs
@@ -7,55 +7,53 @@ using SimpleFeedReader.Services;
 using SimpleFeedReader.ViewModels;
 using Xunit;
 
-namespace SimpleFeedReader.Tests.Services
+namespace SimpleFeedReader.Tests.Services;
+public class NewsServiceTests
 {
-    public class NewsServiceTests
+    private readonly NewsService _newsService;
+
+    public NewsServiceTests()
     {
-        private readonly NewsService _newsService;
+        var mockMapper = new MapperConfiguration(cfg =>
+            cfg.AddProfile(new NewsStoryProfile()));
+        IMapper mapper = mockMapper.CreateMapper();
 
-        public NewsServiceTests()
-        {
-            var mockMapper = new MapperConfiguration(cfg =>
-                cfg.AddProfile(new NewsStoryProfile()));
-            IMapper mapper = mockMapper.CreateMapper();
+        _newsService = new NewsService(mapper);
+    }
 
-            _newsService = new NewsService(mapper);
-        }
+    [Fact]
+    public async Task Returns_News_Stories_Given_Valid_Uri()
+    {
+        // Arrange
+        var feedUrl = "https://azure.microsoft.com/en-us/blog/feed/";
 
-        [Fact]
-        public async Task Returns_News_Stories_Given_Valid_Uri()
-        {
-            // Arrange
-            var feedUrl = "https://azure.microsoft.com/en-us/blog/feed/";
+        // Act
+        List<NewsStoryViewModel> result =
+            await _newsService.GetNews(feedUrl);
 
-            // Act
-            List<NewsStoryViewModel> result =
-                await _newsService.GetNews(feedUrl);
+        // Assert
+        Assert.True(result.Count > 0);
+    }
 
-            // Assert
-            Assert.True(result.Count > 0);
-        }
-       
-        [Fact]
-        public void Throws_UriFormatException_Given_Malformed_Uri()
-        {
-            // Arrange
-            var feedUrl = "invalid_url";
+    [Fact]
+    public void Throws_UriFormatException_Given_Malformed_Uri()
+    {
+        // Arrange
+        var feedUrl = "invalid_url";
 
-            // Act & Assert
-            Assert.ThrowsAsync<UriFormatException>(async () =>
-                await _newsService.GetNews(feedUrl));
-        }
+        // Act & Assert
+        Assert.ThrowsAsync<UriFormatException>(async () =>
+            await _newsService.GetNews(feedUrl));
+    }
 
-        [Fact]
-        public void Throws_WebException_Given_Unknown_Host_Uri()
-        {
-            // Arrange
-            var feedUrl = "https://fail/test.rss";
+    [Fact]
+    public void Throws_WebException_Given_Unknown_Host_Uri()
+    {
+        // Arrange
+        var feedUrl = "https://fail/test.rss";
 
-            // Act & Assert
-            Assert.ThrowsAsync<WebException>(async () =>
-                await _newsService.GetNews(feedUrl));
-        }
+        // Act & Assert
+        Assert.ThrowsAsync<WebException>(async () =>
+            await _newsService.GetNews(feedUrl));
     }
 }
